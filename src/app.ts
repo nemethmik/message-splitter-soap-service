a65b79044e8fed74bc1f5bb2620c5fedb9d93e2b
Nemeth Miklos <nemethmiklos1960@gmail.com>
2020-12-23 11:40:36 +0100

    This is just an initial hello express POC app

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+// import type {Express} from "express"
+// const app: Express = express()
+const app: express.Express = express()
+app.get("/",(req,res) => {
+    res.send("Hello from Node SOAP Service!")
+})
+let port: number = parseInt(process.env.PORT || "8000")
+if(isNaN(port) || port <= 0 || port >= 65536) port = 8000
+app.listen(port,()=>{
+    console.log(`Open http://localhost:${port}/ in a browser to test`)
+})
