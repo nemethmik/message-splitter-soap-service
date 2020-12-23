0c748bb73f2237615f4f8094619932ffa8d76f51
Nemeth Miklos <nemethmiklos1960@gmail.com>
2020-12-23 20:27:23 +0100

    V826 is committed

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express"
 // const app: Express = express()
 const app: express.Express = express()
 app.get("/",(req,res) => {
-    res.send("Hello from Node SOAP Service!")
+    res.send("Hello from Node SOAP Service V826!")
 })
 let port: number = parseInt(process.env.PORT || "8000")
 if(isNaN(port) || port <= 0 || port >= 65536) port = 8000
