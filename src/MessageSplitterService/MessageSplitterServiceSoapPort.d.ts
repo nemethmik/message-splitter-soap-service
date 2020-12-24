22e0d4e63c805b73f1a466196f6ae692f72ca0dc
Nemeth Miklos <nemethmiklos1960@gmail.com>
2020-12-24 22:37:59 +0100

    1st version of WSDL service available

---
+++ src/MessageSplitterService/MessageSplitterServiceSoapPort.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable:max-line-length no-empty-interface */
+import { Client } from 'soap';
+
+export interface IMessageSplitterInput {
+    /** s:string(undefined) */
+    message: string;
+    /** s:string(undefined) */
+    splitter: string;
+}
+
+export interface IMessageSplitterOutput {
+    /** s:string(undefined) */
+    result: string[];
+}
+
+export interface IMessageSplitterServiceSoapPortSoap extends Client {
+    MessageSplitter: (input: Partial<IMessageSplitterInput>, cb: (err: any | null, result: IMessageSplitterOutput, rawResult: string,  soapHeader: {[k: string]: any; },  rawRequest: string) => any, options?: any, extraHeaders?: any) => void;
+    MessageSplitterAsync: (input: Partial<IMessageSplitterInput>, options?: any, extraHeaders?: any) => Promise<[IMessageSplitterOutput, string, {[k: string]: any; }, string]>;
+}
--- src/soapservice.ts
+++ src/soapservice.ts
@@ -1,12 +1,46 @@
 import express from "express"
+import fs from "fs"
+import {listen as soapListen, Server as SoapServer, IServices,ISoapServiceMethod, IService,IServicePort} from "soap"
+import type {IMessageSplitterInput,IMessageSplitterOutput} from "./MessageSplitterService/MessageSplitterServiceSoapPort"
 // import type {Express} from "express"
 // const app: Express = express()
+interface IMessageSplitterServiceSoapPort extends IServicePort {
+    MessageSplitter: ISoapServiceMethod,
+}
+interface IMessageSplitterService extends IService {
+    MessageSplitterServiceSoapPort: IMessageSplitterServiceSoapPort,
+}
+interface IAllMyServicesFromWSDL extends IServices {
+    MessageSplitterService: IMessageSplitterService,
+}
+
 const app: express.Express = express()
+
+const splitter_function: ISoapServiceMethod = (args:IMessageSplitterInput):IMessageSplitterOutput => {
+    console.log('splitter_function')
+    const splitted_msg:string[] = args.message.split(args.splitter)
+    let result = []
+    for(let i = 0; i < splitted_msg.length; i++) result.push(splitted_msg[i])
+    return {result/*,version:"V1012"*/}
+}
+
+const serviceObject:IAllMyServicesFromWSDL = {
+    MessageSplitterService: {
+        MessageSplitterServiceSoapPort: {
+            MessageSplitter: splitter_function
+        },
+        MessageSplitterServiceSoap12Port: {
+            MessageSplitter: splitter_function
+        }
+      }
+  }
+const xml = fs.readFileSync("service.wsdl", "utf8")
 app.get("/",(req,res) => {
-    res.send("Hello from Node SOAP Service V1020!")
+    res.send("Message Splitter SOAP Service V1233!")
 })
 let port: number = parseInt(process.env.PORT || "8000")
 if(isNaN(port) || port <= 0 || port >= 65536) port = 8000
-app.listen(port,()=>{
-    console.log(`Open http://localhost:${port}/ in a browser to test`)
+app.listen(port,() => {
+    /*const soapServer: SoapServer =*/ soapListen(app, "/wsdl", serviceObject, xml)
+    console.log(`Open http://localhost:${port}/wsdl?wsdl in a browser to test`)
 })
